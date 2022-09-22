30b967fc2fbed89e3cb70bf1a73d529265eaab14
Ivan Zolotov <ivanzolotov@ivanzolotov.ru>
2022-09-22 15:04:04 +0500

    Refactor catalog.js

--- imports/ui/catalog/catalog.js
+++ imports/ui/catalog/catalog.js
@@ -5,14 +5,16 @@ import { SETTINGS } from '/imports/settings/setting';
 
 import './catalog.html';
 
+const homepageCategories = () => {
+  const ids = SETTINGS.homepageCategories;
+  const categories = [];
+  for (let i = 0; i < ids.length; i++) {
+    const category = CategoriesCollection.find({id: ids[i].id}).fetch()[0];
+    categories.push(category);
+  }
+  return categories;
+};
+
 Template.templateCatalog.helpers({
-  homepageCategories() {
-    const ids = SETTINGS.homepageCategories;
-    const categories = [];
-    for (let i = 0; i < ids.length; i++) {
-      const category = CategoriesCollection.find({id: ids[i].id}).fetch()[0];
-      categories.push(category);
-    }
-    return categories;
-  },
+  homepageCategories: homepageCategories,
 });
